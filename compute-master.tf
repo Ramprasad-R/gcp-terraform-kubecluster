f4002bccf06b20aea1ca4b5b9fa374718b4ad956
Ramprasad-R <ramprasad.info@gmail.com>
2020-12-30 14:50:11 +0100

    added provisioner test script

--- compute-master.tf
+++ compute-master.tf
@@ -19,6 +19,46 @@ resource "google_compute_instance" "kube-master" {
     }
   }
 
+  #   provisioner "remote-exec" {
+  #     connection {
+  #       type        = "ssh"
+  #       user        = "Techlivz"
+  #       timeout     = "500s"
+  #       private_key = file("gcpkey")
+  #           agent           = "false"
+  #     }
+
+  #     inline = [
+  #       "touch /tmp/temp.txt",
+  #     ]
+  #   }
+
+  #   provisioner "file" {
+  #       connection {
+  #       type        = "ssh"
+  #       user        = "Techlivz"
+  #       timeout     = "500s"
+  #       private_key = file("gcpkey")
+  #           agent           = "false"
+  #     }
+  #     source      = "./scripts"
+  #     destination = "/tmp/"
+  #   }
+  #   provisioner "remote-exec" {
+  #       connection {
+  #       type        = "ssh"
+  #       user        = "Techlivz"
+  #       timeout     = "500s"
+  #       private_key = file("gcpkey")
+  #           agent           = "false"
+  #     }
+  #     inline = [
+  #       "chmod +x /tmp/scripts/*.sh",
+  #       "sudo sh /tmp/scripts/docker_install.sh",
+  #       "sudo sh /tmp/scripts/kubeadm.sh"
+  #       ]
+  #   }
+
   metadata = {
     sshKeys = "Techlivz:${file(var.ssh_public_key_filepath)}"
   }
--- compute-worker.tf
+++ compute-worker.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "kube-worker" {
   name         = "kube-worker-${count.index}"
   machine_type = "n1-standard-2"
   tags         = ["kube-worker-${count.index}"]
-  count = var.worker_count
+  count        = var.worker_count
 
   boot_disk {
     initialize_params {
@@ -18,6 +18,46 @@ resource "google_compute_instance" "kube-worker" {
       // Include this section to give the VM an external ip address
     }
   }
+
+  #   provisioner "remote-exec" {
+  #     connection {
+  #       type        = "ssh"
+  #       user        = "Techlivz"
+  #       timeout     = "500s"
+  #       private_key = file("gcpkey")
+  #           agent           = "false"
+  #     }
+
+  #     inline = [
+  #       "touch /tmp/temp.txt",
+  #     ]
+  #   }
+
+  #   provisioner "file" {
+  #       connection {
+  #       type        = "ssh"
+  #       user        = "Techlivz"
+  #       timeout     = "500s"
+  #       private_key = file("gcpkey")
+  #           agent           = "false"
+  #     }
+  #     source      = "./scripts"
+  #     destination = "/tmp/"
+  #   }
+  #   provisioner "remote-exec" {
+  #       connection {
+  #       type        = "ssh"
+  #       user        = "Techlivz"
+  #       timeout     = "500s"
+  #       private_key = file("gcpkey")
+  #           agent           = "false"
+  #     }
+  #     inline = [
+  #       "chmod +x /tmp/scripts/*.sh",
+  #       "sudo sh /tmp/scripts/docker_install.sh",
+  #       "sudo sh /tmp/scripts/kubeadm.sh"
+  #       ]
+  #   }
 
   metadata = {
     sshKeys = "Techlivz:${file(var.ssh_public_key_filepath)}"
